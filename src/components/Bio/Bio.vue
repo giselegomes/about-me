c640e5af1d66023ec6d7dbb6f46cdf67ba668a19
Gisele Gomes <gselegomes@gmail.com>
2022-11-07 19:17:25 -0300

    feat: adds section bio - dark theme

--- src/components/Bio/Bio.vue
+++ src/components/Bio/Bio.vue
@@ -1,15 +1,34 @@
 <template>
 	<section class="bio">
-        <div class="bio__avatar">
-            <img class="bio__avatar--img" src="https://github.com/giselegomes.png" alt="Avatar da Gisele" />
-            <img class="bio__avatar--stars" src="./../../assets/images/stars.png" alt="Avatar da Gisele" />
+        <div class="avatar">
+            <img class="avatar__img" src="https://github.com/giselegomes.png" alt="Avatar da Gisele" />
+            <span class="avatar__img--border"></span>
         </div>
-
-        <div class="bio__info">
-            <h1 class="bio__">Olá, eu sou a Gisele!</h1>
-            <p>Desenvolvedora Front-end</p>
+        <div class="info">
+            <h1 class="info__title">Olá, eu sou a Gisele!</h1>
+            <ul class="info__list list">
+                <li class="list__item">
+                    <img class="list__emoji" src="./../../assets/images/stars-item.png" alt="" />
+                    Desenvolvedora Front-End
+                </li>
+                <li class="list__item">
+                    <img class="list__emoji" src="./../../assets/images/stars-item.png" alt="" />
+                    Estudante de Análise e Desenvolvimento de Sistemas na UFPR
+                </li>
+                <li class="list__item">
+                    <img class="list__emoji" src="./../../assets/images/stars-item.png" alt="" />
+                    Busco desenvolver aplicações responsivas e acessíveis
+                </li>
+                <li class="list__item">
+                    <img class="list__emoji" src="./../../assets/images/stars-item.png" alt="" />
+                    Desenvolvedora Vue.js
+                </li>
+            </ul>
         </div>
     </section>
+    <footer>
+        <img class="" src="./../../assets/images/arabesco.png" alt="" />
+    </footer>
 </template>
 
 <style lang="scss" scoped>
