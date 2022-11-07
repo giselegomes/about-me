c713936f04efc7acc0c8949338afcb6e2ddc8ec2
Gisele Gomes <gselegomes@gmail.com>
2022-11-07 13:20:02 -0300

    feat: navbar finished

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import Navbar from './components/Navbar/Navbar.vue'
+  import Bio from './components/Bio/Bio.vue'
 </script>
 
 <template>
   <Navbar />
+  <Bio />
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/Bio/Bio.vue
@@ -0,0 +1,17 @@
+<template>
+	<section class="bio">
+        <div class="bio__avatar">
+            <img class="bio__avatar--img" src="https://github.com/giselegomes.png" alt="Avatar da Gisele" />
+            <img class="bio__avatar--stars" src="./../../assets/images/stars.png" alt="Avatar da Gisele" />
+        </div>
+
+        <div class="bio__info">
+            <h1 class="bio__">Olá, eu sou a Gisele!</h1>
+            <p>Desenvolvedora Front-end</p>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    @import './style.scss' 
+</style>
