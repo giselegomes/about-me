8a7a186c1d1c078802be7ab3e7e4a5ca58ff5a0d
Gisele Gomes <gselegomes@gmail.com>
2022-11-08 11:41:59 -0300

    feat: add about-me section

--- src/components/AboutMe/AboutMe.vue
+++ src/components/AboutMe/AboutMe.vue
@@ -1,9 +1,31 @@
 <template>
-	<section id="#about-me" class="about">
-        <h2>Sobre mim</h2>
-        <p>
+	<section id="about" class="about">
+        <h2 class="about__title">Sobre mim</h2>
+        <p class="about__description">
             Tenho 3 anos de experiência como Front-end, utilizando diariamente HTML, CSS(SCSS) e Javascript. Nesses 3 anos tive a oportunidade de trabalhar com Vue.js por 6 meses, e hoje é a minha principal ferramenta de estudos. Prezo por desenvolver aplicações responsivas e acessíveis. Acredito que ter um bom relacionamento com os colegas de equipe gera uma melhor troca de conhecimento e o crescimento pessoal e profissional de todos. 
         </p>
+        <ul class="about__socials">
+            <li class="about__socials--github">
+                <a href="https://github.com/giselegomes" target="_blank">
+                    <img src="./../../assets/images/github.png" alt="ícone do Github com o fundo lilás" />
+                </a>
+            </li>
+            <li class="about__socials--linkedin">
+                <a href="https://www.linkedin.com/in/giselegs/" target="_blank">
+                    <img src="./../../assets/images/linkedin.png" alt="ícone do LinkedIn com o fundo lilás" />
+                </a>
+            </li>
+            <li class="about__socials--twitter">
+                <a href="https://twitter.com/gisele_frontend" target="_blank">
+                     <img src="./../../assets/images/twitter.png" alt="ícone do Twitter com o fundo lilás" />
+                </a>
+            </li>
+            <li class="about__socials--email">
+                <a href="malito:gselegomes@gmail.com" target="_blank">
+                    <img src="./../../assets/images/email.png" alt="ícone de e-mail com o fundo lilás" />
+                </a>
+            </li>
+        </ul>
     </section>
 </template>
 
